771e025d7aea90bc0196c59e914907ac58ea0a90
delbaze <56109313+delbaze@users.noreply.github.com>
2024-08-28 15:19:45 +0200

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config) => {
+    // Ajoutez votre configuration ici
+    config.node = {
+      global: false,
+    };
+
+    return config;
+  },
 };
 
 export default nextConfig;
